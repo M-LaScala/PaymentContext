b3d4dab54e727b5ed9d612bae93fbf5575e911dd
Matheus La Scala <matheus.lascala02@gmail.com>
2024-01-26 10:51:10 -0300

    Adicionar arquivos de projeto.

---
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -0,0 +1,11 @@
+﻿namespace PaymentContext.Domain.Entities
+{
+    public class Subscription
+    {
+        public bool Active { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public DateTime? ExpireDate { get; set; }
+        public List<Payment> Payments { get; set; }
+    }
+}
