ad8157a8b902207e22b8e7397243d26ccb5c0a2e
Matheus La Scala <matheus.lascala02@gmail.com>
2024-01-26 20:55:58 -0300

    Aula 08 - Corrupção no Código

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,15 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            var subscription = new Subscription(null);
+            var student = new Student("Matheus","La Scala", "123123","matheus@gmail.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
