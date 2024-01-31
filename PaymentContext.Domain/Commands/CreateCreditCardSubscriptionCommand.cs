612db44d46a3a33be26934eec7b9e896c84044b8
Matheus La Scala <matheus.lascala02@gmail.com>
2024-01-30 23:23:47 -0300

    Aula 14 - Commands

---
+++ PaymentContext.Domain/Commands/CreateCreditCardSubscriptionCommand.cs
@@ -0,0 +1,31 @@
+﻿using PaymentContext.Domain.Enums;
+
+namespace PaymentContext.Domain.Commands
+{
+    public class CreateCreditCardSubscriptionCommand
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string CardHolderName { get; set; }
+        public string CardNumber { get; set; }
+        public string LastTransactionNumer { get; set; }
+        public string PaymentNumber { get; set; }
+        public DateTime PaidDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public decimal Total { get; set; }
+        public decimal TotalPaid { get; set; }
+        public string Payer { get; set; }
+        public Document PayerDocument { get; set; }
+        public EDocumentType PayerDocumentType { get; set; }
+        public string PayerEmail { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public EDocumentType type { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
