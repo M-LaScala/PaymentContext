8f39b68a00c897a79d27d92d41feebf1c4684c66
Matheus La Scala <matheus.lascala02@gmail.com>
2024-01-31 16:46:49 -0300

    Aula 17 - Repository Pattern

---
+++ PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,11 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Repositories
+{
+    public interface IStudentRepository
+    {
+        bool DocumentExists(string document);
+        bool EmailExistis(string email);
+        void CreateSubscription(Student student);
+    }
+}
